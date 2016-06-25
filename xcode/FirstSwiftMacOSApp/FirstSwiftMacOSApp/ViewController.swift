8420c47de159d0833b095e9e884e68a974be7dcb
zhangjunwei <chinwe122@gmail.com>
2016-06-25 11:31:23 +0800

    [ADD]增加Xcode目录

---
+++ xcode/FirstSwiftMacOSApp/FirstSwiftMacOSApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  FirstSwiftMacOSApp
+//
+//  Created by 张俊伟 on 16/6/25.
+//  Copyright © 2016年 张俊伟. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
