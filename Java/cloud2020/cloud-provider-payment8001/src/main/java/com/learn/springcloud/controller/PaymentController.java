48d60a3e0eac85320b427604c880a17c789d9e83
chinwe <chinwe122@gmail.com>
2020-05-18 22:16:42 +0800

    服务发现配置

--- Java/cloud2020/cloud-provider-payment8001/src/main/java/com/learn/springcloud/controller/PaymentController.java
+++ Java/cloud2020/cloud-provider-payment8001/src/main/java/com/learn/springcloud/controller/PaymentController.java
@@ -1,13 +1,17 @@
 package com.learn.springcloud.controller;
 
+import cn.hutool.log.Log;
 import com.learn.springcloud.entities.CommonResult;
 import com.learn.springcloud.entities.Payment;
 import com.learn.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -18,6 +22,9 @@ public class PaymentController {
 
     @Value("${server.port}")
     private String serverPort;
+
+    @Resource
+    private DiscoveryClient discoveryClient;
 
     @PostMapping(value = "payment/create")
     public CommonResult create(@RequestBody Payment payment) {
@@ -38,5 +45,23 @@ public class PaymentController {
         } else {
             return new CommonResult(404, "No result.");
         }
+    }
+
+    @GetMapping(value = "payment/discovery")
+    public Object discovery() {
+        List<String> services = discoveryClient.getServices();
+        for (String service : services) {
+            Log log = Log.get();
+            log.info("service " + service);
+        }
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+        for (ServiceInstance instance : instances) {
+            Log log = Log.get();
+            log.info(instance.getServiceId() +
+                    "\t" + instance.getHost() +
+                    "\t" + instance.getPort() +
+                    "\t" + instance.getUri());
+        }
+        return this.discoveryClient;
     }
 }
