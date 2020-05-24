81630a9004d5602d42c96e3689a585d11d2d3709
chinwe <chinwe122@gmail.com>
2020-05-24 22:47:15 +0800

    消费服务注册到consul

---
+++ Java/cloud2020/cloud-consumerconsul-order80/src/main/java/springcloud/controller/OrderConsulController.java
@@ -0,0 +1,23 @@
+package springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
+@RestController
+@Slf4j
+public class OrderConsulController {
+
+    public static final String PAYMENT_URL = "http://cloud-payment-service";
+
+    @Resource
+    private RestTemplate restTemplate;
+
+    @GetMapping(value = "consumer/payment/zk")
+    public String paymentInfo() {
+        return restTemplate.getForObject(PAYMENT_URL + "/payment/zk", String.class);
+    }
+}
