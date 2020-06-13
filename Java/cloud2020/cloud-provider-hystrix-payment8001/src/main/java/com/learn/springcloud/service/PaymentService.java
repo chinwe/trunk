e4d6d4c00867af9634b0cab6d94a42c7697f0d5b
chinwe <chinwe122@gmail.com>
2020-06-13 10:12:22 +0800

    hystrix 超时服务降级

--- Java/cloud2020/cloud-provider-hystrix-payment8001/src/main/java/com/learn/springcloud/service/PaymentService.java
+++ Java/cloud2020/cloud-provider-hystrix-payment8001/src/main/java/com/learn/springcloud/service/PaymentService.java
@@ -1,5 +1,7 @@
 package com.learn.springcloud.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
@@ -11,12 +13,19 @@ public class PaymentService {
         return "Thread" + Thread.currentThread().getName() + " paymentInfoOk, id: " + id + ".";
     }
 
+    @HystrixCommand(fallbackMethod = "paymentInfoTimeoutFallback", commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "3000")
+    })
     public String paymentInfoTimeout(Integer id) {
         try {
-            TimeUnit.SECONDS.sleep(3);
+            TimeUnit.SECONDS.sleep(5);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
         return "Thread" + Thread.currentThread().getName() + " paymentInfoTimeout, id: " + id + ".";
+    }
+
+    public String paymentInfoTimeoutFallback(Integer id) {
+        return "Thread" + Thread.currentThread().getName() + " paymentInfoTimeout, id: " + id + ". fallback :(";
     }
 }
