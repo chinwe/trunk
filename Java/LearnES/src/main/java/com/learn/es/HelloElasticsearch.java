eed5eba2d64e3a6557c397eef10e91a58c232feb
chinwe <chinwe122@gmail.com>
2021-05-09 23:14:46 +0800

    elasticsearch java api

---
+++ Java/LearnES/src/main/java/com/learn/es/HelloElasticsearch.java
@@ -0,0 +1,25 @@
+package com.learn.es;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+
+import java.io.IOException;
+
+/**
+ * @author chinwe
+ */
+public class HelloElasticsearch {
+    public static void main(String[] args) throws IOException {
+
+        // 创建ES客户端对象
+        RestHighLevelClient client = new RestHighLevelClient(
+                RestClient.builder(new HttpHost("localhost", 9200, "http"))
+        );
+
+        System.out.println(client);
+
+        // 关闭客户端连接
+        client.close();
+    }
+}
