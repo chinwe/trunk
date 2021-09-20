3fb00e0edb7190bb197b51a2d347262e464942ed
chinwe <chinwe122@gmail.com>
2021-09-20 23:06:51 +0800

    Java8 时间相关练习代码

---
+++ Java/Java8InAction/src/main/java/datatime/DSTMain.java
@@ -0,0 +1,52 @@
+package datatime;
+
+import java.text.SimpleDateFormat;
+import java.time.*;
+import java.util.Date;
+import java.util.Set;
+import java.util.TimeZone;
+
+/**
+ * @author chinwe
+ * 2021/9/20
+ */
+public class DSTMain {
+    public static void main(String[] args) {
+
+        long oneDay = 24 * 60 * 60 * 1000;
+        //2019-10-26 04:00:00
+        long startTime = 1572055200000L;
+        //2019-10-26 06:00:00
+        long endTime = 1572062400000L;
+        SimpleDateFormat bjSdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        TimeZone timeZone = TimeZone.getTimeZone("Europe/Madrid");
+        bjSdf.setTimeZone(timeZone);
+        System.out.println(bjSdf.format(new Date(startTime)));
+        //System.out.println(bjSdf.format(new Date(endTime)));
+        System.out.println(bjSdf.format(new Date(startTime + oneDay)));
+        //System.out.println(bjSdf.format(new Date(endTime + oneDay)));
+
+        // 所有时区
+        final Set<String> availableZoneIds = ZoneId.getAvailableZoneIds();
+        // System.out.println(availableZoneIds);
+
+        // ISO-8601
+        LocalDateTime localDateTime = LocalDateTime.now();
+        OffsetDateTime offsetDateTime = OffsetDateTime.of(localDateTime, ZoneOffset.of("+08:00"));
+        System.out.println("Beijing Time:       " + offsetDateTime);
+
+        // 巴黎时间
+        final LocalDateTime parisNow = LocalDateTime.ofInstant(Instant.now(), ZoneId.of("Europe/Paris"));
+        final OffsetDateTime parisISO = OffsetDateTime.ofInstant(Instant.now(), ZoneId.of("Europe/Paris"));
+        System.out.println("Paris Time:         " + parisISO);
+
+        // America/Los_Angeles
+        final OffsetDateTime isoLosAngeles = OffsetDateTime.ofInstant(Instant.now(), ZoneId.of("America/Los_Angeles"));
+        System.out.println("Los Angeles Time:   " + isoLosAngeles);
+        System.out.println(TimeZone.getTimeZone("America/Los_Angeles"));
+
+        // Epoch Milli
+        System.out.println(Instant.now().toEpochMilli());
+        System.out.println(System.currentTimeMillis());
+    }
+}
