4aa7ca29908dfd13caeb819d07e70375ef3b35c5
chinwe <chinwe122@gmail.com>
2021-09-23 23:59:29 +0800

    Effective Java Items4

--- Java/EffectiveJava/src/main/java/chapter2/Item4.java
+++ Java/EffectiveJava/src/main/java/chapter2/Item4.java
@@ -1,8 +1,20 @@
 package chapter2;
 
 /**
- * @author chinwe
- * 2021/9/23
+ * 第4条：通过私有构造器强化不可实例化的能力
+ *
  */
 public class Item4 {
+
+    public static class UlilityClass {
+        // Suppresses default constructor, ensuring non-instantiability.
+        private UlilityClass() {
+            throw new AssertionError();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        UlilityClass ulilityClass = new UlilityClass();
+    }
 }
