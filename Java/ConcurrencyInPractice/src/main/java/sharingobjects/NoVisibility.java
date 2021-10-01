91cc8b3af2ceb342cf8171e1233ceaf81863d7bf
chinwe <chinwe122@gmail.com>
2021-10-01 12:55:48 +0800

    Java Concurrency In Practice

---
+++ Java/ConcurrencyInPractice/src/main/java/sharingobjects/NoVisibility.java
@@ -0,0 +1,30 @@
+package sharingobjects;
+
+/**
+ * 在没有同步的情况下共享变量
+ *
+ * 只要数据需要被跨线程共享，就进行恰当的同步
+ *
+ * @author chinwe
+ * 2021/10/1
+ */
+public class NoVisibility {
+    private static boolean ready;
+    private static int number;
+
+    private static class ReaderThread extends Thread {
+        @Override
+        public void run() {
+            while (!ready) {
+                Thread.yield();
+            }
+            System.out.println(number);
+        }
+    }
+
+    public static void main(String[] args) {
+        new ReaderThread().start();
+        number = 42;
+        ready = true;
+    }
+}
---
+++ Java/ConcurrencyInPractice/src/main/java/threadsafety/Main.java
@@ -0,0 +1,16 @@
+package threadsafety;
+
+/**
+ * 定义：一个类是线程安全的，是指被多个线程访问时，类可以持续进行正确的行为
+ * 无状态对象永远是线程安全的
+ * 原子变量 atomic variable
+ * 内部锁 synchronized
+ *
+ *
+ * @author chinwe
+ * 2021/10/1
+ */
+public class Main {
+    public static void main(String[] args) {
+    }
+}
