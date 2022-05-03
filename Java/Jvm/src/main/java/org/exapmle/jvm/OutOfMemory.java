eb560576c1e8c3d680c99c4a798e118cada532ed
zhangjunwei <chinwe122@gmail.com>
2022-05-03 12:22:44 +0800

    测试提交

---
+++ Java/Jvm/src/main/java/org/exapmle/jvm/OutOfMemory.java
@@ -0,0 +1,12 @@
+package org.exapmle.jvm;
+
+/**
+ * @author mozixun
+ * @description
+ * @date 2022/5/3 - 12:11
+ */
+public class OutOfMemory {
+    public static void main(String[] args) {
+        System.out.println("out of memory");
+    }
+}
