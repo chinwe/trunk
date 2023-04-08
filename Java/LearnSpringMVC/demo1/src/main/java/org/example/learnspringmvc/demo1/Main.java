2264b143ee2c580a9e6632672e12bd7e3d63e035
chinwe <chinwe122@gmail.com>
2023-04-08 17:51:06 +0800

    LearnSpringMVC

---
+++ Java/LearnSpringMVC/demo1/src/main/java/org/example/learnspringmvc/demo1/Main.java
@@ -0,0 +1,11 @@
+package org.example.learnspringmvc.demo1;
+
+/**
+ * @author chinwe
+ * 2023/4/8
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
