f4886f3728fea6a206dfe9638f4742e62bc6b013
chinwe <chinwe122@gmail.com>
2024-01-26 21:45:27 +0800

    Java 新版本特性

---
+++ Java/JavaNewFetures/java9/src/main/java/collection/CollectionNew.java
@@ -0,0 +1,29 @@
+package collection;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * 在集合上，Java 9 增加 了 List.of()、Set.of()、Map.of() 和 Map.ofEntries()等工厂方法来创建不可变集合。
+ *
+ * @author chinwe
+ * 2024/1/26
+ */
+public class CollectionNew {
+    public static void main(String[] args) {
+        System.out.println(List.of());
+        System.out.println(List.of("Hello", "World"));
+        System.out.println(List.of(1, 2, 3));
+
+        System.out.println(Set.of());
+        System.out.println(Set.of("Hello", "World"));
+        System.out.println(Set.of(1, 2, 3));
+
+        System.out.println(Map.of());
+        System.out.println(Map.of("Hello", 1, "World", 2));
+
+        // 对不可变集合操作会抛出异常 java.lang.UnsupportedOperationException
+
+    }
+}
