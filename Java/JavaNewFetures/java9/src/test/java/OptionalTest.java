abd5c6c9ad6a2b15766e7d839df7266cfbe85522
chinwe <chinwe122@gmail.com>
2024-01-27 19:59:51 +0800

    Java 新版本特性

--- Java/JavaNewFetures/java9/src/test/java/OptionalTest.java
+++ Java/JavaNewFetures/java9/src/test/java/OptionalTest.java
@@ -1,6 +1,27 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 /**
+ * Optional 类中新增了 ifPresentOrElse、or 和 stream 等方法。
+ *
  * @author chinwe
  * 2024/1/27
  */
-public class OptionalTest {
+class OptionalTest {
+
+    @Test
+    void testStream() {
+        final long count = Stream.of(
+                        Optional.of(1),
+                        Optional.empty(),
+                        Optional.of(2),
+                        Optional.empty().or(() -> Optional.of(3))
+                ).flatMap(Optional::stream)
+                .count();
+        assertEquals(3, count);
+    }
 }
---
+++ Java/JavaNewFetures/java9/src/test/java/ProcessHandleTest.java
@@ -0,0 +1,27 @@
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.time.Duration;
+
+/**
+ * Java 9 增加了 ProcessHandle 接口，可以对原生进程进行管理，尤其适合于管理长时间运行的进程。
+ *
+ * @author chinwe
+ * 2024/1/27
+ */
+class ProcessHandleTest {
+
+    @Test
+    void testProcessHandle() throws IOException {
+        final ProcessBuilder processBuilder = new ProcessBuilder("java", "-version")
+                .inheritIO();
+        final ProcessHandle processHandle = processBuilder.start().toHandle();
+        processHandle.onExit().whenCompleteAsync((handle, throwable) -> {
+            if (throwable == null) {
+                System.out.println(handle.pid());
+            } else {
+                throwable.printStackTrace();
+            }
+        });
+    }
+}
