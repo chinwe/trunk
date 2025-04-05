c550912d301d95836b40d3f233381ab68a4a9141
chinwe <chinwe122@163.com>
2025-04-05 12:50:20 +0800

    fastapi demo

---
+++ python/fastapi-demo/test_main.py
@@ -0,0 +1,13 @@
+from fastapi.testclient import TestClient
+
+from main import app
+
+client = TestClient(app)
+
+
+def test_hello():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {
+        "Hello": "World"
+    }
