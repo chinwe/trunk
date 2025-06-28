09f6c9a10a3b6c625421e94265465cca0665b76e
chinwe <chinwe122@gmail.com>
2025-06-28 08:54:41 +0800

    Add WaitGroup to synchronize goroutines in test
    This commit modifies the TestGroutine function in groutine_test.go to use a sync.WaitGroup. This ensures that the main test function waits for all goroutines to complete before finishing, preventing potential race conditions and ensuring reliable test output.

--- go/src/go_learning/src/ch16/groutine/groutine_test.go
+++ go/src/go_learning/src/ch16/groutine/groutine_test.go
@@ -2,13 +2,18 @@ package groutine
 
 import (
 	"fmt"
+	"sync"
 	"testing"
 )
 
 func TestGroutine(t *testing.T) {
-	for i := 0; i < 10; i++ {
+	var wg sync.WaitGroup
+	for i := range 10 {
+		wg.Add(1)
 		go func(i int) {
+			defer wg.Done()
 			fmt.Println(i)
 		}(i)
 	}
+	wg.Wait()
 }
